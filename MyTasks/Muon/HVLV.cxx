a7a8ca121256fb3a4824aaadcfeeb0db252a1d1a
comrademarvin <stephan.stefie@gmail.com>
2024-02-21 13:16:33 +0200

    Find Timestamp ranges of HV objects for runs

--- MyTasks/Muon/HVLV.cxx
+++ MyTasks/Muon/HVLV.cxx
@@ -1,4 +1,5 @@
 #include "CCDB/CcdbApi.h"
+#include "CCDB/BasicCCDBManager.h"
 #include "DetectorsDCS/DataPointIdentifier.h"
 #include "DetectorsDCS/DataPointValue.h"
 #include "DetectorsDCS/DataPointCreator.h"
@@ -13,6 +14,8 @@
 #include <numeric>
 #include <vector>
 #include <set>
+#include <fstream>
+#include <sstream>
 #include "TGraph.h"
 #include "TFile.h"
 #include "TCanvas.h"
@@ -21,26 +24,57 @@
 using DPID = o2::dcs::DataPointIdentifier;
 using DPVAL = o2::dcs::DataPointValue;
 using DPMAP = std::unordered_map<DPID, std::vector<DPVAL>>;
+using RBMAP = std::map<int, std::pair<uint64_t, uint64_t>>;
+using HVBMAP = std::map<uint64_t, uint64_t>;
 
-float sum(float s, o2::dcs::DataPointValue v)
-{
-    union Converter {
-        uint64_t raw_data;
-        double value;
-    } converter;
-    converter.raw_data = v.payload_pt1;
-    return s + converter.value;
-};
+float sum(float s, o2::dcs::DataPointValue v);
+std::set<int> GetRuns(std::string runList);
+RBMAP GetRunBoundaries(o2::ccdb::CcdbApi const& api, std::string runList);
+void PrintRunBoundaries(const RBMAP& runBoundaries);
+std::string GetTime(uint64_t ts);
+uint64_t MSToS(uint64_t ts);
+HVBMAP GetHVBoundaries(o2::ccdb::CcdbApi const& api, uint64_t tStart, uint64_t tStop);
+void PrintHVBoundaries(const HVBMAP& hvBoundaries);
+
 
 int main() {
-    TGraph* HV_entries = new TGraph();
+    //TGraph* HV_entries = new TGraph();
 
+    // read in runlist from textfile
+    std::ifstream runfile("runlist.txt");
+    std::stringstream buffer;
+    buffer << runfile.rdbuf();
+
+    std::string runlist = buffer.str();
+
+    // Access CCDB API
     o2::ccdb::CcdbApi api;
 
     string ccdbUrl = "http://alice-ccdb.cern.ch";
     api.init(ccdbUrl);
 
+    // look at run boundaries
+    auto runBoundaries = GetRunBoundaries(api, runlist);
+
+    if (runBoundaries.empty()) {
+        printf("no run found from the list\n");
+        return 0;
+    }
+
+    PrintRunBoundaries(runBoundaries);
+
+    // extract the timestamp boundaries for each HV file in the full time range
+    auto hvBoundaries = GetHVBoundaries(api, runBoundaries.begin()->second.first, runBoundaries.rbegin()->second.second);
+    PrintHVBoundaries(hvBoundaries);
+
+    /*
+    // fetch HV object in timestamp range
     std::map<std::string, std::string> metadata;
+
+    for (auto runs : runBoundaries) {
+
+    }
+
     uint64_t timestamp = 1698299830000;
     auto* HV_map = api.retrieveFromTFileAny<DPMAP>("MCH/Calib/HV", metadata, timestamp);
 
@@ -108,7 +142,138 @@ int main() {
         }
     }
 
-    delete outFile;
+    delete outFile; */
     
     return 0;
+}
+
+float sum(float s, o2::dcs::DataPointValue v)
+{
+    union Converter {
+        uint64_t raw_data;
+        double value;
+    } converter;
+    converter.raw_data = v.payload_pt1;
+    return s + converter.value;
+};
+
+RBMAP GetRunBoundaries(o2::ccdb::CcdbApi const& api, std::string runList)
+{
+  /// return the SOR / EOR time stamps for every runs in the list
+
+  RBMAP runBoundaries{};
+
+  auto runs = GetRuns(runList);
+
+  for (auto run : runs) {
+    auto boundaries = o2::ccdb::CCDBManagerInstance::getRunDuration(api, run);
+    runBoundaries.emplace(run, boundaries);
+  }
+
+  return runBoundaries;
+}
+
+std::set<int> GetRuns(std::string runList)
+{
+  /// read the runList from an ASCII file, or a comma separated run list, or a single run
+
+  std::set<int> runs{};
+
+  auto isNumber = [](std::string val) { return !val.empty() && val.find_first_not_of("0123456789") == val.npos; };
+
+  if (isNumber(runList)) {
+
+    runs.insert(std::stoi(runList));
+
+  } else if (runList.find(",") != runList.npos) {
+
+    std::istringstream input(runList);
+    for (std::string run; std::getline(input, run, ',');) {
+      if (isNumber(run)) {
+        runs.insert(std::stoi(run));
+      }
+    }
+
+  } else {
+
+    std::ifstream input(runList);
+    if (input.is_open()) {
+      for (std::string run; std::getline(input, run);) {
+        if (isNumber(run)) {
+          runs.insert(std::stoi(run));
+        }
+      }
+    }
+  }
+
+  return runs;
+}
+
+void PrintRunBoundaries(const RBMAP& runBoundaries)
+{
+  /// print the list of runs with their time boundaries
+
+  printf("\nlist of runs with their boundaries:\n");
+  printf("------------------------------------\n");
+
+  for (const auto& [run, boundaries] : runBoundaries) {
+    printf("%d: %lld - %lld (%s - %s)\n", run, boundaries.first, boundaries.second,
+           GetTime(boundaries.first).c_str(), GetTime(boundaries.second).c_str());
+  }
+
+  printf("------------------------------------\n");
+}
+
+std::string GetTime(uint64_t ts)
+{
+  /// convert the time stamp (ms) to local time
+
+  time_t t = MSToS(ts);
+
+  std::string time = std::ctime(&t);
+  time.pop_back(); // remove trailing \n
+
+  return time;
+}
+
+uint64_t MSToS(uint64_t ts)
+{
+  /// convert the time stamp from ms to s
+
+  return (ts + 500) / 1000;
+}
+
+HVBMAP GetHVBoundaries(o2::ccdb::CcdbApi const& api, uint64_t tStart, uint64_t tStop)
+{
+  /// get the time boundaries of every HV files found in the time range
+
+  // add extra margin (ms) of ± 1 min to the creation time, which occurs every 30 min
+  static const uint64_t timeMarging[2] = {60000, 1860000};
+
+  HVBMAP hvBoundaries{};
+
+  std::istringstream fileInfo(api.list("MCH/Calib/HV", false, "text/plain",
+                                       tStop + timeMarging[1], tStart - timeMarging[0]));
+
+  for (std::string line; std::getline(fileInfo, line);) {
+    if (line.find("Validity:") == 0) {
+      hvBoundaries.emplace(std::stoull(line.substr(10, 13)), std::stoull(line.substr(26, 13)));
+    }
+  }
+
+  return hvBoundaries;
+}
+
+void PrintHVBoundaries(const HVBMAP& hvBoundaries)
+{
+  /// print the time boundaries of every HV files found in the full time range
+
+  printf("\nlist of HV file time boundaries:\n");
+  printf("------------------------------------\n");
+
+  for (auto [tStart, tStop] : hvBoundaries) {
+    printf("%lld - %lld (%s - %s)\n", tStart, tStop, GetTime(tStart).c_str(), GetTime(tStop).c_str());
+  }
+
+  printf("------------------------------------\n");
 }
--- MyTasks/Muon/build.sh
+++ MyTasks/Muon/build.sh
@@ -1,3 +1,4 @@
-cd /home/overlay_sw/BUILD/O2-latest/O2/
+cd /home/overlay_sw/BUILD/O2-latest-dev/O2/
 ninja MyTasks/Muon/install
 cd /scratch/ptgjak001/o2hpc/alice/O2/MyTasks/Muon/
+o2-mch-hvlv-ccdb
