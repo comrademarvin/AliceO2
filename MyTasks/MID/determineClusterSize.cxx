26aca138c2918a7eccfe28ed4ad004c1fd777189
comrademarvin <stephan.stefie@gmail.com>
2025-06-04 13:45:21 +0000

    adding MCH to MID cluster workflow

--- MyTasks/MID/determineClusterSize.cxx
+++ MyTasks/MID/determineClusterSize.cxx
@@ -26,31 +26,38 @@
 #include "MIDBase/Mapping.h"
 #include "MIDBase/GeometryTransformer.h"
 #include "MIDClustering/PreClusterizer.h"
+#include "DataFormatsMCH/ROFRecord.h"
+#include "DataFormatsMCH/TrackMCH.h"
 
 std::tuple<TFile *, TTreeReader *> loadData(const char *fileName, const char *treeName);
 
 int main() {
-    std::cout << "Hello World, Cluster Size Task Exists!" << std::endl;
-
-    // read in the track and digit infomation
+    // read in the MID/Muon track and digit infomation
     auto [digitFile, digitReader] = loadData("mid-digits-decoded.root", "middigits");
-    auto [recoFile, recoReader] = loadData("mid-reco.root", "midreco");
+    auto [recoFileMID, recoReaderMID] = loadData("mid-reco.root", "midreco");
+    auto [recoFileMCH, recoReaderMCH] = loadData("mchtracks.root", "mchtracks");
+    auto [recoFileMuon, recoReaderMuon] = loadData("muontracks.root", "muontracks");
 
+    // MID
     TTreeReaderValue<std::vector<o2::mid::ColumnData>> digits{*digitReader, "MIDDigit"};
     TTreeReaderValue<std::vector<o2::mid::ROFRecord>> digitRofs{*digitReader, "MIDROFRecords"};
-    // TTreeReaderValue<std::vector<o2::mid::Cluster>> clusters{*recoReader, "MIDTrackCluster"};
-    // TTreeReaderValue<std::vector<o2::mid::ROFRecord>> clusterRofs{*recoReader, "MIDTrackClusterROF"};
-    TTreeReaderValue<std::vector<o2::mid::Track>> tracks{*recoReader, "MIDTrack"};
-    TTreeReaderValue<std::vector<o2::mid::ROFRecord>> trackRofs{*recoReader, "MIDTrackROF"};
+    // TTreeReaderValue<std::vector<o2::mid::Cluster>> clusters{*recoReaderMID, "MIDTrackCluster"};
+    // TTreeReaderValue<std::vector<o2::mid::ROFRecord>> clusterRofs{*recoReaderMID, "MIDTrackClusterROF"};
+    TTreeReaderValue<std::vector<o2::mid::Track>> tracksMID{*recoReaderMID, "MIDTrack"};
+    TTreeReaderValue<std::vector<o2::mid::ROFRecord>> trackRofsMID{*recoReaderMID, "MIDTrackROF"};
 
-    if (digitReader->GetEntries() != recoReader->GetEntries()) // same number of TFs
+    // MCH
+    TTreeReaderValue<std::vector<o2::mch::TrackMCH>> tracksMCH{*recoReaderMCH, "tracks"};
+    TTreeReaderValue<std::vector<o2::mch::ROFRecord>> trackRofsMCH{*recoReaderMCH, "trackrofs"};
+
+    if ((digitReader->GetEntries() != recoReaderMID->GetEntries()) && (digitReader->GetEntries() != recoReaderMCH->GetEntries())) // same number of TFs
     {
         std::cout << "Error: the digit and cluster readers do not contain the same number of TFs";
         exit(-1);
     }
 
     // output histograms
-    TH1D* nTracksROF = new TH1D("ROF_track_count", "Number of Tracks per ROF;nTracks/ROF;count", 50, 0, 50);
+    TH1D* nTracksROF_MID = new TH1D("ROF_track_count", "Number of MID Tracks per ROF;nTracks/ROF;count", 50, 0, 50);
     TH1D* nClusterStrips = new TH1D("cluster_strips_size", "Number of Strips in MID Clusters;nStrips/Cluster;count", 30, 0, 30);
 
     // re-run preclusterizer on event digits
@@ -58,19 +65,20 @@ int main() {
 
     // itterate over entries, where each entry is a TF
     int entriesCount = 0;
-    while (digitReader->Next() && recoReader->Next()) {
+    while (digitReader->Next() && recoReaderMID->Next() && recoReaderMCH->Next()) {
         //auto clusterRofIt = (*clusterRofs).begin(); // itterator over cluster ROFs for one TF
-        auto trackRofIt = (*trackRofs).begin(); // itterator over track ROFs for one TF
+        auto trackRofItMID = (*trackRofsMID).begin(); // itterator over MID track ROFs for one TF
+        auto trackRofItMCH = (*trackRofsMCH).begin(); // itterator over MCH track ROFs for one TF
 
         gsl::span<o2::mid::ColumnData> sdigits(*digits); // all digits for the TF
 
         // itterate over digit ROFs for one TF
         for (auto digitRofIt = (*digitRofs).begin(), digitEnd = (*digitRofs).end(); digitRofIt != digitEnd; ++digitRofIt)
         {
-            auto nTracks = trackRofIt->nEntries; // number of tracks for the ROF
-            if (nTracks > 0) { // check whether there are any tracks for the ROF
-                //std::cout << "nTracks of ROF: " << nTracks << std::endl;
-                nTracksROF->Fill(nTracks);
+            auto nTracksMID = trackRofItMID->nEntries; // number of MID tracks for the ROF
+            auto nTracksMCH = trackRofItMCH->nEntries; // number of MCH tracks for the ROF
+            if ((nTracksMID > 0) && (nTracksMCH > 0)) { // check whether there are any tracks for the ROF
+                nTracksROF_MID->Fill(nTracksMID);
 
                 // subspan of digits for the ROF where there are tracks
                 auto eventDigits = sdigits.subspan(digitRofIt->firstEntry, digitRofIt->nEntries);
@@ -89,7 +97,7 @@ int main() {
             }
 
             //++clusterRofIt;
-            ++trackRofIt;
+            ++trackRofItMID;
         }
         
         entriesCount++;
@@ -101,7 +109,7 @@ int main() {
     auto outFile = new TFile("cluster_size.root", "RECREATE");
 
     TCanvas* nTracksCanvas = new TCanvas("ROF_tracks_count", "ROF_tracks_count");
-    nTracksROF->Draw();
+    nTracksROF_MID->Draw();
     nTracksCanvas->Write();
 
     TCanvas* nStripsCanvas = new TCanvas("cluster_strip_size", "cluster_strip_size");
