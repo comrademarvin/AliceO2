0ea136aeae2406d1676aa6fcb0573e26c31c9763
comrademarvin <stephan.stefie@gmail.com>
2025-11-14 15:58:01 +0000

    added fitting function per cluster hist

--- MyTasks/MID/determineClusterSize.cxx
+++ MyTasks/MID/determineClusterSize.cxx
@@ -74,9 +74,8 @@ void fillStripPositions(clusterSizeHist* hist, int nStrips, int pitch) {
     double clusterPos; // position in mm
     // second approach
     for (int strip = 0; strip < nStrips; strip++) {
-        int bin = strip + 1; // Bins are 1-indexed in ROOT histograms
+        int bin = strip; // Bins are 1-indexed in ROOT histograms
         double binCenter = hist->clusterPosition->GetBinCenter(bin);
-        hist->clusterPosition->Fill(binCenter);
         hist->clusterPosition->Fill(binCenter);
     }
 
@@ -134,135 +133,78 @@ void processClusterHist(std::vector<clusterSizeHist*> clusterSizeHistograms) {
         return;
     }
 
-    // add histograms with same cathode and deId together
-    std::map<std::tuple<int, int>, std::pair<TH1F*, int>> combinedHists; // key: (cathode, deId), value: pair of combined histogram and cluster count
-    for (auto& hist : clusterSizeHistograms) {
-        auto key = std::make_tuple(hist->cathode, hist->deId);
-        if (combinedHists.find(key) == combinedHists.end()) {
-            combinedHists[key] = std::make_pair(
-                (TH1F*)hist->clusterPosition->Clone(Form("combined_cathode%i_deId%i", hist->cathode, hist->deId)),
-                hist->nClusters
-            );
-            combinedHists[key].first->SetTitle(Form("Combined Strip Position (mm) for Cathode %i, DE %i", hist->cathode, hist->deId));
-        } else {
-            combinedHists[key].first->Add(hist->clusterPosition);
-            combinedHists[key].second += hist->nClusters;
-        }
-    }
-
-    auto testKey = std::make_tuple(0, 3); // using cathode = 0, deId = 0 as a test
-    (combinedHists[testKey].first)->Scale(1.0 / (static_cast<float>(combinedHists[testKey].second)), "width"); // normalize by number of clusters
-
-    // pick test histogram for now (to fit parameters A,B,C)
-    auto baseClusterHist = clusterSizeHistograms[343]; // index 240: deId = 48, cathode = 0, pitch = 1 | index 343: deId = 68, cathode = 1, pitch = 2
-    baseClusterHist->clusterPosition->Scale(1.0 / (static_cast<float>(baseClusterHist->nClusters)), "width"); // normalize by number of clusters
-    //baseClusterHist->clusterPosition->Scale(1 / (2*baseClusterHist->clusterPosition->Integral())); // normalize by area + for overcounting due to symmetry assumption
-
-
-    // create current O2 PDF for the chosen hist (using default HV for now)
-    o2::mid::ChamberResponse chamberResp = o2::mid::createDefaultChamberResponse();
-
-    const int nPoints = 100; // Number of points in the grid
-    double distances[nPoints];
-    double firedProbabilities[nPoints];
-
-    for (int i = 0; i < nPoints; ++i) {
-        distances[i] = i; // Distance values from 0 to 100 in steps of 1 in mm
-        firedProbabilities[i] = chamberResp.getFiredProbability(distances[i]/10, baseClusterHist->cathode, baseClusterHist->deId, 0.0); // need to pass in cm
-    }
-
-    TGraph* graph = new TGraph(nPoints, distances, firedProbabilities);
-
-    // access HV values from the CCDB
-    //auto ccdb = o2::ccdb::BasicCCDBManager::getInstance();
-
-    // define my own fit function
-    auto clusterPDF = new TF1("clusterPDF", pdfFunc, 0, 100, 6);
-    clusterPDF->SetParNames("b", "a0", "a1", "c0", "c1", "hv");
-    clusterPDF->SetParameters(1.97, -52.70, 6.089, -0.5e-3, 8.3e-4, 9.6); // initial parameters
-    clusterPDF->FixParameter(5, 9.6); // Fix the high-voltage parameter
-
-    //baseClusterHist->clusterPosition->Fit("clusterPDF", "R", "", 0, 100); // fit in range of 0-100 mm
+    // Access CCDB API to retrieve HV values
+    // o2::ccdb::CcdbApi api;
+    // string ccdbUrl = "http://alice-ccdb.cern.ch";
+    // api.init(ccdbUrl);
 
     // read out histograms
     auto outFile = new TFile("cluster_hist_fitting.root", "RECREATE");
 
-    // plot first hist and current PDF together as a sanity check
-    TCanvas* canvasCheckFirst = new TCanvas("FiredProbabilityCanvas", "Fired Probability vs Distance", 800, 600);
-    gPad->SetLogy();
-    baseClusterHist->clusterPosition->SetLineColor(kBlack);
-    baseClusterHist->clusterPosition->SetMaximum(1.0);
-    baseClusterHist->clusterPosition->SetMinimum(0.0001);
-    baseClusterHist->clusterPosition->Draw("SAME");
+    // create current O2 PDF for the chosen hist (using default HV for now)
+    o2::mid::ChamberResponse chamberResp = o2::mid::createDefaultChamberResponse();
 
-    graph->Draw("SAME");
-    graph->SetLineColor(kRed);
-    graph->SetLineWidth(2);
+    // convenient test histograms
+    // index 240: deId = 48, cathode = 0, pitch = 1
+    // index 340: deId = 68, cathode = 0, pitch = 1
+    // index 343: deId = 68, cathode = 1, pitch = 2
+    // index 324: deId = 68, cathode = 1, pitch = 4
+    
+    for (auto clusterHist : clusterSizeHistograms) {
+        if (clusterHist->nClusters == 0) continue; // skip empty histograms
 
-    canvasCheckFirst->Write();
+        clusterHist->clusterPosition->Scale(1.0 / (static_cast<float>(clusterHist->nClusters))); // normalize by number of clusters
 
-    // plot test combined histogram with fit function for different b-parameter values
-    TCanvas* canvasCheckCombine = new TCanvas("FiredProbabilityCanvasCombinedB", "Fired Probability vs Distance for cathode = 0 and pitch = 1", 800, 600);
-    gPad->SetLogy();
-    (combinedHists[testKey].first)->SetLineColor(kBlack);
-    (combinedHists[testKey].first)->SetMaximum(1.0);
-    (combinedHists[testKey].first)->SetMinimum(0.0001);
-    (combinedHists[testKey].first)->Draw("SAME");
+        // set up probability function graph for plotting
+        const int nPoints = 100; // Number of points in the grid
+        double distances[nPoints];
+        double firedProbabilities[nPoints];
 
-    double b_parameter_values[] = {1.97, 2.22, 2.47, 2.97}; // plot the PDF for different b-parameter values
-    int colors[] = {kRed, kBlue, kGreen, kMagenta};
-    TLegend* legend = new TLegend(0.7, 0.7, 0.9, 0.9);
-    legend->SetHeader("b-Parameter Values", "C");
+        // Get the range of the current histogram
+        double xMin = clusterHist->clusterPosition->GetXaxis()->GetXmin();
+        double xMax = clusterHist->clusterPosition->GetXaxis()->GetXmax();
+        double step = (xMax - xMin) / (nPoints - 1); // Step size for the grid
 
-    for (int i = 0; i < 4; ++i) {
-        auto tempPDF = new TF1("tempPDF", pdfFunc, 0, 100, 6);
-        tempPDF->SetParNames("b", "a0", "a1", "c0", "c1", "hv");
-        tempPDF->SetParameters(b_parameter_values[i], -52.70, 6.089, -0.5e-3, 8.3e-4, 9.6); // initial parameters
-        tempPDF->SetLineColor(colors[i]);               
-        tempPDF->SetLineWidth(2);                      
-        tempPDF->Draw("SAME"); // Overlay subsequent functions
-        legend->AddEntry(tempPDF, Form("b = %.2f", b_parameter_values[i]), "l");
+        for (int i = 0; i < nPoints; ++i) {
+            distances[i] = xMin + i * step; // Distance values spanning the histogram range
+            firedProbabilities[i] = chamberResp.getFiredProbability(distances[i] / 10, clusterHist->cathode, clusterHist->deId, 0.0); // need to pass in cm
+        }
+
+        TGraph* graph = new TGraph(nPoints, distances, firedProbabilities);
+
+        // access high-voltage value from CCDB (for the chamber?)
+
+
+        // define my own fit function
+        auto clusterPDF = new TF1("clusterPDF", pdfFunc, 0, 100, 6);
+        clusterPDF->SetParNames("b", "a0", "a1", "c0", "c1", "hv");
+        clusterPDF->SetParameters(1.97, -52.70, 6.089, -0.5e-3, 8.3e-4, 9.6); // initial parameters
+        //for (int i = 1; i < 6; ++i) clusterPDF->FixParameter(i, clusterPDF->GetParameter(i)); // fix all except 'b'
+        clusterPDF->FixParameter(5, 9.6); // fix only HV parameter for now
+
+        // fit the histogram with the PDF
+        double fitMin = clusterHist->clusterPosition->GetBinLowEdge(1); // Lower edge of the first bin
+        double fitMax = clusterHist->clusterPosition->GetBinLowEdge(6); // Lower edge of the sixth bin (end of the first 5 bins)
+        clusterHist->clusterPosition->Fit("clusterPDF", "R", "", fitMin, fitMax);
+
+        // plot first hist, current PDF, and fitted function together
+        TCanvas* canvasCheckFirst = new TCanvas(Form("strip_position_de%i_cathode%i_pitch%i", clusterHist->deId, clusterHist->cathode, clusterHist->pitch), "Fired Probability vs Distance", 800, 600);
+        gPad->SetLogy();
+        clusterHist->clusterPosition->SetLineColor(kBlack);
+        clusterHist->clusterPosition->SetMaximum(1.0);
+        clusterHist->clusterPosition->SetMinimum(0.0001);
+        clusterHist->clusterPosition->Draw("SAME");
+
+        graph->Draw("SAME");
+        graph->SetLineColor(kRed);
+        graph->SetLineWidth(2);
+
+        clusterPDF->SetLineColor(kBlue);
+        clusterPDF->SetLineWidth(2);
+        clusterPDF->Draw("SAME");
+
+        canvasCheckFirst->Write();
     }
-    legend->Draw("SAME");
-
-    canvasCheckCombine->Write();
-
-    // plot test combined histogram with fit function for different HV values
-    TCanvas* canvasCheckCombineHV = new TCanvas("FiredProbabilityCanvasCombinedHV", "Fired Probability vs Distance for cathode = 0 and pitch = 1", 800, 600);
-    gPad->SetLogy();
-    (combinedHists[testKey].first)->SetLineColor(kBlack);
-    (combinedHists[testKey].first)->SetMaximum(1.0);
-    (combinedHists[testKey].first)->SetMinimum(0.0001);
-    (combinedHists[testKey].first)->Draw("SAME");
-
-    double hv_values[] = {9.2, 9.4, 9.6, 9.8}; // plot the PDF for different hv values
-    TLegend* legendHV = new TLegend(0.7, 0.7, 0.9, 0.9);
-    legendHV->SetHeader("HV Values (mV)", "C");
-
-    for (int i = 0; i < 4; ++i) {
-        auto tempPDF = new TF1("tempPDF", pdfFunc, 0, 100, 6);
-        tempPDF->SetParNames("b", "a0", "a1", "c0", "c1", "hv");
-        tempPDF->SetParameters(1.97, -52.70, 6.089, -0.5e-3, 8.3e-4, hv_values[i]); // initial parameters
-        tempPDF->SetLineColor(colors[i]);               
-        tempPDF->SetLineWidth(2);                      
-        tempPDF->Draw("SAME"); // Overlay subsequent functions
-        legendHV->AddEntry(tempPDF, Form("HV = %.1f", hv_values[i]), "l");
-    }
-    legendHV->Draw("SAME");
-
-    canvasCheckCombineHV->Write();
-
-    int histIndex = 0;
-    for (auto& hist : clusterSizeHistograms) {
-        hist->clusterPosition->Write(Form("strip_position_de%i_cathode%i_pitch%i_index%i", hist->deId, hist->cathode, hist->pitch, histIndex));
-        histIndex++;
-    }
-
-    // for (const auto& [key, value] : combinedHists) {
-    //     int cathode = std::get<0>(key);
-    //     int deId = std::get<1>(key);
-    //     value.first->Write(Form("combined_strip_position_de%i_cathode%i", deId, cathode));
-    // }
 
     delete outFile;
 }
@@ -504,7 +446,7 @@ std::vector<clusterSizeHist*> initializeClusterSizeHist() {
                 Double_t binEdges[nBins + 1] = {0.0, 0.5*stripWidth, 1.0*stripWidth, 1.5*stripWidth, 2.0*stripWidth, 2.5*stripWidth, 
                                                 3.0*stripWidth, 3.5*stripWidth, 4.0*stripWidth, 4.5*stripWidth, 5.0*stripWidth}; // fraction of strip widths for bins
                 hist->clusterPosition = new TH1F(Form("strip_position_de%i_cathode%i_pitch%i", deId, cathode, indexToPitch(pitch)), 
-                                                Form("Strip Position (mm) for DE %i, Cathode %i, Pitch %i", deId, cathode, indexToPitch(pitch)), nBins, binEdges);
+                                                Form("Strip Position (mm) for DE %i, Cathode %i, Pitch %i, Chamber %i", deId, cathode, indexToPitch(pitch), hist->chamber), nBins, binEdges);
                 clusterSizeHistograms.push_back(hist);
             }
         }
