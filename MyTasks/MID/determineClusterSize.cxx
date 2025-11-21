b19acdd2020c00802c526f34cdfa67d3004d7a65
comrademarvin <stephan.stefie@gmail.com>
2025-11-21 11:11:27 +0000

    adding HV from CCDB

--- MyTasks/MID/determineClusterSize.cxx
+++ MyTasks/MID/determineClusterSize.cxx
@@ -31,7 +31,14 @@
 #include "MIDClustering/PreClusterizer.h"
 #include "MIDClustering/PreCluster.h"
 #include "MIDSimulation/ChamberResponse.h"
+#include "MIDSimulation/ChamberResponseParams.h"
+#include "MIDConditions/DCSNamer.h"
 #include "ReconstructionDataFormats/TrackMCHMID.h"
+
+// CCDB HV data formats
+using DPID = o2::dcs::DataPointIdentifier;
+using DPVAL = o2::dcs::DataPointValue;
+using DPMAP = std::unordered_map<DPID, std::vector<DPVAL>>;
 
 const int nPitches = 3;
 
@@ -51,6 +58,9 @@ std::vector<o2::mid::PreCluster> processMIDdigits(const char *fileMIDdigits, con
 std::vector<clusterSizeHist*> processPreClusters(std::vector<o2::mid::PreCluster> preClustersMID);
 void processClusterHist(std::vector<clusterSizeHist*> clusterSizeHistograms);
 void fillStripPositions(clusterSizeHist* hist, int nStrips, int pitch);
+DPMAP* accessHVObjectCCDB(int runNumber);
+float accessHVperDE(DPMAP* HV_map, int deId);
+float sum(float s, o2::dcs::DataPointValue v);
 
 int pitchToIndex(int pitch);
 int indexToPitch(int index);
@@ -58,7 +68,7 @@ int indexToPitch(int index);
 int main() {
     auto muonTracksIR = processMuonTracks("muontracks.root");
 
-    //auto preClustersMID = processMIDdigits("mid-digits-decoded.root", "mid-reco.root", muonTracksIR);
+    //auto preClustersMID = processMIDdigits("mid-digits-decoded.root", "mid-reco.root", muonTracksIR); // for data
     auto preClustersMID = processMIDdigits("middigits.root", "mid-reco.root", muonTracksIR); // for MC
 
     auto clusterSizeHist = processPreClusters(preClustersMID);
@@ -78,6 +88,8 @@ void fillStripPositions(clusterSizeHist* hist, int nStrips, int pitch) {
         double binCenter = hist->clusterPosition->GetBinCenter(bin);
         hist->clusterPosition->Fill(binCenter);
     }
+
+    hist->nClusters++;
 
     // first approach    
     // div_t nStripsDiv2 = div(nStrips, 2);
@@ -111,20 +123,19 @@ void fillStripPositions(clusterSizeHist* hist, int nStrips, int pitch) {
     //     //     if (strip != nStripsDiv2.quot) hist->clusterPosition->Fill(clusterPos); // fill once for shifted asymmetry of cluster position distribution
     //     // }
     // }
-
-    hist->nClusters++;
 }
 
 Double_t pdfFunc(Double_t *x, Double_t *par) // x = position (mm); par = {b, a0, a1, c0, c1, hv}
 {
     Float_t xx = x[0];
     double hv = par[5];
+    double costheta = std::cos(par[6]);
 
     double a = par[2]*hv + par[1]; // a = a1*hv + a0
     double c = par[4]*hv + par[3]; // c = c1*hv + c0
     double b = par[0];
 
-    return (1/(1+c))*((a/(a+pow(xx,b)))+c);
+    return (1/(1+c)) * ((a / (a + (pow(xx,b)*costheta))) + c);
 }
 
 void processClusterHist(std::vector<clusterSizeHist*> clusterSizeHistograms) {
@@ -133,59 +144,46 @@ void processClusterHist(std::vector<clusterSizeHist*> clusterSizeHistograms) {
         return;
     }
 
-    // Access CCDB API to retrieve HV values
-    // o2::ccdb::CcdbApi api;
-    // string ccdbUrl = "http://alice-ccdb.cern.ch";
-    // api.init(ccdbUrl);
+    // MID HV information
+    DPMAP* HV_map = accessHVObjectCCDB(558801);
 
     // read out histograms
     auto outFile = new TFile("cluster_hist_fitting.root", "RECREATE");
 
-    // create current O2 PDF for the chosen hist (using default HV for now)
-    o2::mid::ChamberResponse chamberResp = o2::mid::createDefaultChamberResponse();
-
-    // convenient test histograms
-    // index 240: deId = 48, cathode = 0, pitch = 1
-    // index 340: deId = 68, cathode = 0, pitch = 1
-    // index 343: deId = 68, cathode = 1, pitch = 2
-    // index 324: deId = 68, cathode = 1, pitch = 4
+    // create current O2 PDF and parameters for comparison and fitting
+    o2::mid::ChamberResponseParams chamberRespParam = o2::mid::createDefaultChamberResponseParams();
     
     for (auto clusterHist : clusterSizeHistograms) {
         if (clusterHist->nClusters == 0) continue; // skip empty histograms
 
         clusterHist->clusterPosition->Scale(1.0 / (static_cast<float>(clusterHist->nClusters))); // normalize by number of clusters
 
-        // set up probability function graph for plotting
-        const int nPoints = 100; // Number of points in the grid
-        double distances[nPoints];
-        double firedProbabilities[nPoints];
+        // HV for the deID
+        float HV_value = accessHVperDE(HV_map, clusterHist->deId);
 
         // Get the range of the current histogram
         double xMin = clusterHist->clusterPosition->GetXaxis()->GetXmin();
         double xMax = clusterHist->clusterPosition->GetXaxis()->GetXmax();
-        double step = (xMax - xMin) / (nPoints - 1); // Step size for the grid
 
-        for (int i = 0; i < nPoints; ++i) {
-            distances[i] = xMin + i * step; // Distance values spanning the histogram range
-            firedProbabilities[i] = chamberResp.getFiredProbability(distances[i] / 10, clusterHist->cathode, clusterHist->deId, 0.0); // need to pass in cm
-        }
-
-        TGraph* graph = new TGraph(nPoints, distances, firedProbabilities);
-
-        // access high-voltage value from CCDB (for the chamber?)
-
+        // define current PDF for comparison
+        auto clusterPDF_current = new TF1(Form("clusterPDF_current_de%i_cathode%i_pitch%i", clusterHist->deId, clusterHist->cathode, clusterHist->pitch), pdfFunc, 0, xMax, 7);
+        clusterPDF_current->SetParNames("b", "a0", "a1", "c0", "c1", "hv", "theta");
+        clusterPDF_current->SetParameters(chamberRespParam.getParB(clusterHist->cathode, clusterHist->deId), -52.70, 6.089, -0.5e-3, 8.3e-4, HV_value, 0.0); // current parameters
+        for (int i = 0; i < 7; ++i) clusterPDF_current->FixParameter(i, clusterPDF_current->GetParameter(i)); // fix all parameters (for comparison only)
 
         // define my own fit function
-        auto clusterPDF = new TF1("clusterPDF", pdfFunc, 0, 100, 6);
-        clusterPDF->SetParNames("b", "a0", "a1", "c0", "c1", "hv");
-        clusterPDF->SetParameters(1.97, -52.70, 6.089, -0.5e-3, 8.3e-4, 9.6); // initial parameters
-        //for (int i = 1; i < 6; ++i) clusterPDF->FixParameter(i, clusterPDF->GetParameter(i)); // fix all except 'b'
-        clusterPDF->FixParameter(5, 9.6); // fix only HV parameter for now
+        auto clusterPDF_fit = new TF1(Form("clusterPDF_fit_de%i_cathode%i_pitch%i", clusterHist->deId, clusterHist->cathode, clusterHist->pitch), pdfFunc, 0, xMax, 7);
+        clusterPDF_fit->SetParNames("b", "a0", "a1", "c0", "c1", "hv", "theta");
+        clusterPDF_fit->SetParameters(chamberRespParam.getParB(clusterHist->cathode, clusterHist->deId), -52.70, 6.089, -0.5e-3, 8.3e-4, HV_value, 0.0); // initial parameters
+        //for (int i = 0; i < 7; ++i) clusterPDF_fit->FixParameter(i, clusterPDF_fit->GetParameter(i)); // fix all parameters (for comparison only)
+        //for (int i = 1; i < 7; ++i) clusterPDF_fit->FixParameter(i, clusterPDF_fit->GetParameter(i)); // fix all except 'b'
+        clusterPDF_fit->FixParameter(5, HV_value); // fix HV parameter
+        clusterPDF_fit->FixParameter(6, 0.0); // fix theta parameter
 
         // fit the histogram with the PDF
         double fitMin = clusterHist->clusterPosition->GetBinLowEdge(1); // Lower edge of the first bin
-        double fitMax = clusterHist->clusterPosition->GetBinLowEdge(6); // Lower edge of the sixth bin (end of the first 5 bins)
-        clusterHist->clusterPosition->Fit("clusterPDF", "R", "", fitMin, fitMax);
+        double fitMax = clusterHist->clusterPosition->GetBinLowEdge(17); // Lower edge of the seventeenth bin (end of the first 16 bins)
+        clusterHist->clusterPosition->Fit(Form("clusterPDF_fit_de%i_cathode%i_pitch%i", clusterHist->deId, clusterHist->cathode, clusterHist->pitch), "R", "", fitMin, fitMax);
 
         // plot first hist, current PDF, and fitted function together
         TCanvas* canvasCheckFirst = new TCanvas(Form("strip_position_de%i_cathode%i_pitch%i", clusterHist->deId, clusterHist->cathode, clusterHist->pitch), "Fired Probability vs Distance", 800, 600);
@@ -195,18 +193,64 @@ void processClusterHist(std::vector<clusterSizeHist*> clusterSizeHistograms) {
         clusterHist->clusterPosition->SetMinimum(0.0001);
         clusterHist->clusterPosition->Draw("SAME");
 
-        graph->Draw("SAME");
-        graph->SetLineColor(kRed);
-        graph->SetLineWidth(2);
+        clusterPDF_current->SetLineColor(kRed);
+        clusterPDF_current->SetLineWidth(2);
+        clusterPDF_current->DrawClone("SAME");
 
-        clusterPDF->SetLineColor(kBlue);
-        clusterPDF->SetLineWidth(2);
-        clusterPDF->Draw("SAME");
+        clusterPDF_fit->SetLineColor(kBlue);
+        clusterPDF_fit->SetLineWidth(2);
+        clusterPDF_fit->DrawClone("SAME");
+
+        // Add a legend
+        TLegend* legend = new TLegend(0.6, 0.7, 0.9, 0.9);
+        legend->AddEntry(clusterHist->clusterPosition, "Data (Run 3)", "l");
+        legend->AddEntry(clusterPDF_current, "Current O2 PDF (Run 2)", "l");
+        legend->AddEntry(clusterPDF_fit, "Fitted PDF", "l");
+        legend->Draw("SAME");
 
         canvasCheckFirst->Write();
     }
 
     delete outFile;
+}
+
+float accessHVperDE(DPMAP* HV_map, int deId) {
+    std::string deAlias = o2::mid::dcs::detElemId2DCSAlias(deId, o2::mid::dcs::MeasurementType::HV_V);
+    for (auto const& [key, values] : *HV_map) {
+        std::string entry_alias = key.get_alias();
+        if (entry_alias == deAlias) {
+            // std::cout << "HV values for DE " << deId << ": ";
+            // for (auto val : values) {
+            //     double HV_value = sum(0.0, val);
+            //     std::cout << HV_value << ", ";
+            // }
+            // std::cout << std::endl;
+            float HV_value = sum(0.0, values.back())/1000; // return the last HV value (in kV) - for now!
+            //std::cout << "Accessed HV for DE " << deId << ": " << HV_value << " kV" << std::endl;
+            return HV_value;
+        }
+    }
+}
+
+DPMAP* accessHVObjectCCDB(int runNumber) {
+    // Access CCDB API to retrieve HV values
+    o2::ccdb::CcdbApi api;
+    std::string ccdbUrl = "http://alice-ccdb.cern.ch";
+    api.init(ccdbUrl);
+
+    // access info about the relevant run from the CCDB
+    std::pair<uint64_t, uint64_t> runBoundaries = o2::ccdb::CCDBManagerInstance::getRunDuration(api, runNumber);
+
+    // access HV values for run from CCDB
+    std::map<std::string, std::string> metadata;
+    DPMAP* HV_map = api.retrieveFromTFileAny<DPMAP>("MID/Calib/HV", metadata, runBoundaries.first);
+
+    // for (auto const& [key, values] : *HV_map) {
+    //     std::string entry_alias = key.get_alias();
+    //     std::cout << "HV entry alias: " << entry_alias << std::endl;
+    // }
+
+    return HV_map;
 }
 
 std::vector<clusterSizeHist*> processPreClusters(std::vector<o2::mid::PreCluster> preClustersMID) {
@@ -235,32 +279,39 @@ std::vector<clusterSizeHist*> processPreClusters(std::vector<o2::mid::PreCluster
         // determine strip size 
         int nStrips;
         if (pc.cathode == 0) { // is the cluster in the bending plane?
+            if (pc.firstColumn != pc.lastColumn) continue;
+            //if (pc.firstColumn != 4) continue; // check only one column for testing
+
             strip_pitch_first = static_cast<int>(2*mpArea_first.getHalfSizeY());
             strip_pitch_last = static_cast<int>(2*mpArea_last.getHalfSizeY());
 
-            nStrips = (int)(pc.lastStrip - pc.firstStrip + 16 * (pc.lastLine - pc.firstLine)) + 1;
+            if (strip_pitch_first != strip_pitch_last) continue;
 
-            if (strip_pitch_first == strip_pitch_last) nStripsClusterBending[pitchToIndex(strip_pitch_first)]->Fill(nStrips);
+            nStrips = ((int)pc.lastStrip - (int)pc.firstStrip) + (16 * ((int)pc.lastLine - (int)pc.firstLine)) + 1;
+            // if (((int)pc.lastLine - (int)pc.firstLine) > 1) std::cout << "nStrips: " << nStrips << ", First line: " << (int)pc.firstLine << ", Last line: " << (int)pc.lastLine 
+            //                                                 << ", First strip: " << (int)pc.firstStrip << ", Last strip: " << (int)pc.lastStrip << ", in DE: " << (int)pc.deId << std::endl;
+
+            nStripsClusterBending[pitchToIndex(strip_pitch_first)]->Fill(nStrips);
         } 
         else {
             strip_pitch_first = static_cast<int>(2*mpArea_first.getHalfSizeX());
             strip_pitch_last = static_cast<int>(2*mpArea_last.getHalfSizeX());
+            
+            if (strip_pitch_first != strip_pitch_last) continue;
 
             nStrips = (pc.lastStrip - pc.firstStrip) + 1;
             for (int column = pc.firstColumn; column < pc.lastColumn; column++) {
                 nStrips += mapper->getNStripsNBP(column, pc.deId);
             }
 
-            if (strip_pitch_first == strip_pitch_last) nStripsClusterNonBending[pitchToIndex(strip_pitch_first)]->Fill(nStrips);
+            nStripsClusterNonBending[pitchToIndex(strip_pitch_first)]->Fill(nStrips);
         }
 
         // fill cluster size histograms
-        if (strip_pitch_first == strip_pitch_last) { // only when the strip pitch is the same for the full cluster
-            for (auto& hist : clusterSizeHistograms) {
-                if (hist->deId == pc.deId && hist->cathode == pc.cathode && hist->pitch == strip_pitch_first) {
-                    fillStripPositions(hist, nStrips, strip_pitch_first);
-                    break;
-                }
+        for (auto& hist : clusterSizeHistograms) {
+            if (hist->deId == pc.deId && hist->cathode == pc.cathode && hist->pitch == strip_pitch_first) {
+                fillStripPositions(hist, nStrips, strip_pitch_first);
+                break;
             }
         }
     }
@@ -284,7 +335,7 @@ std::vector<o2::mid::PreCluster> processMIDdigits(const char *fileMIDdigits, con
     TH1D* nMIDTracksROF = new TH1D("mid_track_count_ROF", "Number of MID tracks per ROF;nTracks/ROF;count", 70, 0, 70);
 
     // read in the MID track and digit infomation
-    //auto [digitFile, digitReader] = loadData(fileMIDdigits, "middigits");
+    //auto [digitFile, digitReader] = loadData(fileMIDdigits, "middigits"); // for data
     auto [digitFile, digitReader] = loadData(fileMIDdigits, "o2sim"); // for MC
 
     auto [recoFileMID, recoReaderMID] = loadData(fileMIDtracks, "midreco");
@@ -442,11 +493,10 @@ std::vector<clusterSizeHist*> initializeClusterSizeHist() {
                 hist->nClusters = 0;
 
                 Double_t stripWidth = o2::mid::geoparams::getStripUnitPitchSize(hist->chamber) * static_cast<Double_t>(hist->pitch) * 10; // in mm
-                const Int_t nBins = 10;
-                Double_t binEdges[nBins + 1] = {0.0, 0.5*stripWidth, 1.0*stripWidth, 1.5*stripWidth, 2.0*stripWidth, 2.5*stripWidth, 
-                                                3.0*stripWidth, 3.5*stripWidth, 4.0*stripWidth, 4.5*stripWidth, 5.0*stripWidth}; // fraction of strip widths for bins
+                const Int_t nBins = 2 * 32 / hist->pitch; // number of bins for the histogram
                 hist->clusterPosition = new TH1F(Form("strip_position_de%i_cathode%i_pitch%i", deId, cathode, indexToPitch(pitch)), 
-                                                Form("Strip Position (mm) for DE %i, Cathode %i, Pitch %i, Chamber %i", deId, cathode, indexToPitch(pitch), hist->chamber), nBins, binEdges);
+                                                Form("Strip Position (mm) for DE %i, Cathode %i, Pitch %i, Chamber %i", deId, cathode, indexToPitch(pitch), hist->chamber), nBins, 0.0, (nBins/2) * stripWidth);
+                hist->clusterPosition->Sumw2();
                 clusterSizeHistograms.push_back(hist);
             }
         }
@@ -454,3 +504,12 @@ std::vector<clusterSizeHist*> initializeClusterSizeHist() {
 
     return clusterSizeHistograms;
 }
+
+float sum(float s, o2::dcs::DataPointValue v) {
+    union Converter {
+        uint64_t raw_data;
+        double value;
+    } converter;
+    converter.raw_data = v.payload_pt1;
+    return s + converter.value;
+};
